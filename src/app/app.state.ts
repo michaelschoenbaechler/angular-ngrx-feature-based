70a0c9d2787c767b36c00d56bb44a15d85287392
Michael Schönbächler <m.schoenbaechler@icloud.com>
2020-11-22 14:13:31 +0100

    extend state interface with router

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,5 +1,7 @@
 import * as fromUser from './user/ngrx/user.reducer';
+import * as fromRouter from './app-router.reducer';
 
 export interface AppState {
+    router: fromRouter.State;
     user: fromUser.State;
 }
